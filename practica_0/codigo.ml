096834498fa00151d89eca37bf2e6417949f6058
Diego Trabazo Sardón <dtrabazo@gmail.com>
2015-04-04 13:33:44 +0200

    Computer Theory assignments. Initial import.

---
+++ practica_0/codigo.ml
@@ -0,0 +1,292 @@
+(***************)
+(* Ejercicio 1 *)
+(***************)
+let rec mapdoble f1 f2 = function
+| [h1] -> f1 h1 :: []
+| h1::h2::t -> (f1 h1) :: (f2 h2) :: mapdoble f1 f2 t
+| [] -> []
+;;
+(* val mapdoble : ('a -> 'b) -> ('a -> 'b) -> 'a list -> 'b list = <fun> *)
+
+mapdoble (function x -> x) (function x -> -x) [1;1;1;1;1];;
+(* - : int list = [1; -1; 1; -1; 1] *)
+
+(* mapdoble (function x -> x*2) (function x -> "x") [1;2;3;4;5];; *)
+(* Error de tipos, la segunda función trata strings y la primera ints, nótese
+ * que las funcíones van ambas de 'a -> 'b, esto es, deben tratar los mismos
+ * tipos *)
+
+let y = function x -> 5 in mapdoble y;;
+(* - : ('_a -> int) -> '_a list -> int list = <fun> *)
+
+(***************)
+(* Ejercicio 2 *)
+(***************)
+let rec primero_que_cumple f = function
+| h::t -> if f h then h else primero_que_cumple f t
+| _ -> failwith "No_such_element"
+;;
+(* val primero_que_cumple : ('a -> bool) -> 'a list -> 'a = <fun> *)
+
+primero_que_cumple (function x -> x>0) [-1; -5; 0; 10];;
+(* - : int = 10 *)
+
+let existe f l =
+  try let _ = primero_que_cumple f l in true
+  with Failure "No_such_element" -> false;;
+(* val existe : ('a -> bool) -> 'a list -> bool = <fun> *)
+
+existe (function x -> x>0) [-1; -5; 0; 10];;
+(* - : bool = true *)
+
+existe (function x -> x>0) [-1; -5; 0; -3];;
+(* - : bool = false *)
+
+(* Por la definición de conjunto se asume que no hay elementos repetidos y que
+ * tampoco hay orden entre los elementos *)
+let rec asociado set key = match set with
+  | [] -> failwith "No_such_element"
+  | (a,b)::t ->
+    try
+      primero_que_cumple (function e -> e = key) [a]; b
+    with Failure "No_such_element" -> asociado t key
+;;
+(* val asociado : ('a * 'b) list -> 'a -> 'b = <fun>*)
+
+asociado [('a',1); ('b',2); ('c',3); ('d',4)] 'd';;
+(* - : int = 4 *)
+
+asociado [('a',1); ('b',2); ('c',3); ('d',4)] 'e';;
+(* Exception: Failure "No_such_element". *)
+
+(***************)
+(* Ejercicio 3 *)
+(***************)
+
+type 'a arbol_binario =
+    Vacio
+  | Nodo of 'a * 'a arbol_binario * 'a arbol_binario
+;;
+
+let t = Nodo(3,
+  Nodo(2, Vacio, Vacio),
+  Nodo(5, Nodo(4, Vacio, Vacio), Nodo(1, Vacio, Vacio)))
+;;
+
+let t0 = Vacio;;
+
+(* Rama derecha, nodo, rama izquierda *)
+let rec in_orden = function
+  | Vacio -> []
+  | Nodo (tag, left, right) -> (in_orden left) @ [tag] @ (in_orden right)
+;;
+(* val in_orden : 'a arbol_binario -> 'a list = <fun> *)
+
+in_orden t;;
+(* - : int list = [2; 3; 4; 5; 1] *)
+
+let rec pre_orden = function
+  | Vacio -> []
+  | Nodo (tag, left, right) -> [tag] @ (pre_orden left) @ (pre_orden right)
+;;
+(* val pre_orden : 'a arbol_binario -> 'a list = <fun> *)
+
+pre_orden t;;
+
+let rec post_orden = function
+  | Vacio -> []
+  | Nodo (tag, left, right) -> (post_orden left) @ (post_orden right) @ [tag]
+;;
+(* val post_orden : 'a arbol_binario -> 'a list = <fun> *)
+
+post_orden t;;
+
+let anchura tree =
+  let rec aux tree_ queue = match tree_,queue with
+    Vacio,[] -> []
+  | Vacio,h::t-> aux h t
+  | Nodo (tag, left, right),[] -> tag :: aux left [right]
+  | Nodo (tag, left, right),h::t -> tag :: aux h (left :: right :: t)
+in aux tree []
+;;
+(* val anchura : 'a arbol_binario -> 'a list = <fun> *)
+
+(***************)
+(* Ejercicio 4 *)
+(***************)
+
+type 'a conjunto = Conjunto of 'a list;;
+
+let conjunto_vacio = Conjunto [];;
+(* val conjunto_vacio : 'a conjunto = Conjunto [] *)
+
+let set0 = Conjunto [];;
+let set1 = Conjunto ['a'; 'b'];;
+let set2 = Conjunto ["uno"; "dos"; "tres"];;
+let set3 = Conjunto [1; 2];;
+let set6 = Conjunto [1; 2; 3; 4; 5; 6];;
+let set7 = Conjunto [-1; 0; 1; 2];;
+
+let rec pertenece element = function
+  | Conjunto e ->
+    match e with
+      | [] -> false
+      | h::t -> if h=element then true else pertenece element (Conjunto t)
+;;
+(* val pertenece : 'a -> 'a conjunto -> bool = <fun> *)
+
+pertenece 1 set0;;
+(* pertenece 1 set1;; *)
+pertenece "uno" set2;;
+pertenece "cinco" set2;;
+
+let agregar element set =
+  if pertenece element set then set
+  else match set with
+    | Conjunto e ->
+      match e with
+        | [] -> Conjunto [element]
+        | h::t -> Conjunto (element::h::t)
+;;
+(* val agregar : 'a -> 'a conjunto -> 'a conjunto = <fun> *)
+
+(* agregar 1 set1;; *)
+agregar 1 set3;;
+agregar 1 set0;;
+agregar 3 set3;;
+
+let conjunto_of_list list =
+  let rec aux set = function
+    | [] -> set
+    | h::t -> let set_ = agregar h set in aux set_ t
+  in aux conjunto_vacio list
+;;
+(* val conjunto_of_list : 'a list -> 'a conjunto = <fun> *)
+
+let set4 = conjunto_of_list [1;2;3;4;5;6];;
+let set5 = conjunto_of_list [1;1;1;1];;
+(* set5 = conjunto_of_list ['a'];; *) (* Error de tipos*)
+
+let suprimir element set =
+  let rec aux nuevo_set old_set = match nuevo_set,old_set with
+    | Conjunto [], Conjunto [] -> conjunto_vacio
+    | Conjunto (h::t), Conjunto [] -> nuevo_set
+    | _, Conjunto (h::t) ->
+      if h=element then aux nuevo_set (Conjunto t)
+      else aux (agregar h nuevo_set) (Conjunto t)
+  in aux conjunto_vacio set
+;;
+(* val suprimir : 'a -> 'a conjunto -> 'a conjunto = <fun> *)
+
+let rec cardinal = function
+  | Conjunto [] -> 0
+  | Conjunto (h::t) -> 1 + cardinal (Conjunto t)
+;;
+(* val cardinal : 'a conjunto -> int = <fun> *)
+
+let union set1 set2 =
+  let rec aux nuevo_set set1 set2 = match set1,set2 with
+    | Conjunto [], Conjunto []             -> nuevo_set
+    | Conjunto (h1::t1), Conjunto (h2::t2) ->
+      let nuevo_set = agregar h1 nuevo_set in
+      let nuevo_set = agregar h2 nuevo_set in
+      aux nuevo_set (Conjunto t1) (Conjunto t2)
+    | Conjunto [], Conjunto (h::t)
+    | Conjunto (h::t), Conjunto []         ->
+      aux (agregar h nuevo_set) (Conjunto t) conjunto_vacio
+  in aux conjunto_vacio set1 set2
+;;
+(* val union : 'a conjunto -> 'a conjunto -> 'a conjunto = <fun> *)
+
+union set3 set6;;
+(* - : int conjunto = Conjunto [6; 5; 4; 3; 2; 1] *)
+
+let interseccion set1 set2 =
+  let rec aux nuevo_set set1_ set2_ = match set1_,set2_ with
+    | Conjunto [], Conjunto []             -> nuevo_set
+    | Conjunto (h1::t1), Conjunto (h2::t2) ->
+      if pertenece h1 set2 then let nuevo_set = agregar h1 nuevo_set in
+        if pertenece h2 set1 then let nuevo_set = agregar h2 nuevo_set in
+          aux nuevo_set (Conjunto t1) (Conjunto t2)
+        else aux nuevo_set (Conjunto t1) (Conjunto t2)
+      else aux nuevo_set (Conjunto t1) (Conjunto t2)
+    | Conjunto [], Conjunto (h::t)
+    | Conjunto (h::t), Conjunto []         ->
+      if pertenece h set1 && pertenece h set2 then
+        let nuevo_set = agregar h nuevo_set in
+        aux nuevo_set (Conjunto t) conjunto_vacio
+      else nuevo_set
+  in aux conjunto_vacio set1 set2
+;;
+(* val interseccion : 'a conjunto -> 'a conjunto -> 'a conjunto = <fun> *)
+
+interseccion set3 set6;;
+(* - : int conjunto = Conjunto [2; 1] *)
+interseccion set6 set6;;
+(* - : int conjunto = Conjunto [6; 5; 4; 3; 2; 1] *)
+
+let diferencia set1 set2 =
+  let inters = interseccion set1 set2 in
+  let rec aux nuevo_set = function
+    | Conjunto [] -> nuevo_set
+    | Conjunto (h::t) -> aux (suprimir h nuevo_set) (Conjunto t)
+  in aux set1 inters
+;;
+(* val diferencia : 'a conjunto -> 'a conjunto -> 'a conjunto = <fun> *)
+
+diferencia set6 set7;;
+(* )- : int conjunto = Conjunto [3; 4; 5; 6] *)
+
+let rec incluido set1 set2 = match set1 with
+  | Conjunto [] -> true
+  | Conjunto (h::t) -> pertenece h set2 && incluido (Conjunto t) set2
+;;
+(* val incluido : 'a conjunto -> 'a conjunto -> bool = <fun> *)
+
+let igual set1 set2 =
+  if cardinal set1 = cardinal set2 then
+    let rec aux = function
+      | Conjunto [] -> true
+      | Conjunto (h::t) -> pertenece h set2 && aux (Conjunto t)
+    in aux set1
+  else false
+;;
+(* val igual : 'a conjunto -> 'a conjunto -> bool = <fun> *)
+
+let rec producto_cartesiano elem set new_set =
+match set with
+  | Conjunto [] -> new_set
+  | Conjunto (h::t) ->
+    producto_cartesiano elem (Conjunto t) (agregar (elem,h) new_set)
+;;
+let producto_cartesiano set1 set2 =
+  let rec aux new_set = function
+    | Conjunto [] -> new_set
+    | Conjunto (h::t) ->
+      let new_set_ = producto_cartesiano h set2 new_set
+      in aux new_set_ (Conjunto t)
+  in aux (Conjunto []) set1
+;;
+(* val producto_cartesiano : 'a conjunto -> 'b conjunto -> ('a * 'b) conjunto =
+  <fun> *)
+
+producto_cartesiano set1 set2;;
+(*  - : (char * string) conjunto = Conjunto
+   [('b', "tres"); ('b', "dos"); ('b', "uno"); ('a', "tres"); ('a', "dos");
+    ('a', "uno")] *)
+
+let rec list_of_conjunto = function
+  | Conjunto [] -> []
+  | Conjunto (h::t) -> h::(list_of_conjunto (Conjunto t))
+;;
+(* val list_of_conjunto : 'a conjunto -> 'a list = <fun> *)
+
+set1;;
+(* - : char conjunto = Conjunto ['a'; 'b'] *)
+list_of_conjunto set1;;
+(* - : char list = ['a'; 'b'] *)
+
+set6;;
+(* - : int conjunto = Conjunto [1; 2; 3; 4; 5; 6] *)
+list_of_conjunto set6;;
+(* - : int list = [1; 2; 3; 4; 5; 6] *)
---
+++ practica_1/ejercicios.ml
@@ -0,0 +1,245 @@
+(* Make ocaml-talf library functions available in ocaml top-level with: *)
+(* #load "talf.cma";; *)
+(* Or compile from inside ocaml-talf source directory: 
+ * ocamlc *.cma ejercicios.ml -o ejercicios *)
+
+
+(******************************************************************************)
+(**************************** Automata definitions ****************************)
+(******************************************************************************)
+(* All states *)
+let q0 = Auto.Estado "0";;
+let q1 = Auto.Estado "1";;
+let q2 = Auto.Estado "2";;
+let q3 = Auto.Estado "3";;
+
+(* Available simbols *)
+let s1 = Auto.Terminal "a";;
+let s2 = Auto.Terminal "b";;
+let s3 = Auto.Terminal "c";;
+
+(* Available transitions *)
+let a1 = Auto.Arco_af (q0, q1, s1);;
+let a2 = Auto.Arco_af (q1, q1, s2);;
+let a3 = Auto.Arco_af (q1, q2, s1);;
+let a4 = Auto.Arco_af (q2, q0, Auto.Terminal "");;
+let a5 = Auto.Arco_af (q2, q3, Auto.Terminal "");;
+let a6 = Auto.Arco_af (q2, q3, s1);;
+
+(* States as a set *)
+let q_states = Conj.Conjunto [q0; q1; q2; q3];;
+
+(* Available simbols as a set *)
+let sigma = Conj.Conjunto [s1; s2; s3];;
+
+let initial_state = q0;;
+
+(* Transitions as a set *)
+let transitions = Conj.Conjunto [a1; a2; a3; a4; a5; a6];;
+
+(* End states *)
+let end_states = Conj.Conjunto [q0; q3];;
+
+(* Automata definition au1 *)
+let au1 = Auto.Af (q_states, sigma, initial_state, transitions, end_states);;
+
+(* Automata definition au2 *)
+let a1 = Auto.Arco_af (q0, q1, s1);;
+let a2 = Auto.Arco_af (q1, q1, s2);;
+let a3 = Auto.Arco_af (q1, q2, s1);;
+let a4 = Auto.Arco_af (q2, q0, s3);;
+let a5 = Auto.Arco_af (q2, q3, s1);; 
+let a6 = Auto.Arco_af (q2, q3, s3);;
+let transitions = Conj.Conjunto [a1; a2; a3; a4; a5; a6];;
+let au2 = Auto.Af (q_states, sigma, initial_state, transitions, end_states);;
+
+(* Automata definition au3 *)
+let a1 = Auto.Arco_af (q0, q1, s1);;
+let a2 = Auto.Arco_af (q0, q2, s1);;
+let a3 = Auto.Arco_af (q1, q2, s1);;
+let a4 = Auto.Arco_af (q2, q0, s3);;
+let a5 = Auto.Arco_af (q2, q3, s1);;
+let a6 = Auto.Arco_af (q2, q3, s2);;
+let transitions = Conj.Conjunto [a2; a3; a4; a5; a6];;
+let au3 = Auto.Af (q_states, sigma, initial_state, transitions, end_states);;
+
+
+(******************************************************************************)
+(************************** Some auxiliar functions ***************************)
+(******************************************************************************)
+(* get_arcs : draws conjunto of arcs from an automata *)
+let get_arcs (autom:Auto.af) = match autom with
+  | Auto.Af (_,_,_,t,_) -> t
+;;
+
+(* get_arc_symbol : draws the symbol in an arc definition *)
+let get_arc_symbol = function
+  | Auto.Arco_af (_,_,s) -> s
+;;
+
+(* get_arc_symbol : draws the initial node in an arc definition *)
+let get_arc_initial_node = function
+  | Auto.Arco_af (inn,_,_) -> inn
+;;
+
+(* get_arc_symbol : draws the end node in an arc definition *)
+let get_arc_end_node = function
+  | Auto.Arco_af (_,enn,_) -> enn
+;;
+
+(* get_states : draws conjunto of states from an automata *)
+let get_states (autom:Auto.af) = match autom with
+  | Auto.Af (states,_,_,_,_) -> states
+;;
+
+(* get_end_state : draws initial state from an automata *)
+let get_end_state (autom:Auto.af) = match autom with
+  | Auto.Af (_,_,_,_,end_state) -> end_state
+;;
+
+(* get_initial_state : draws conjunto of end states from an automata *)
+let get_initial_state (autom:Auto.af) = match autom with
+  | Auto.Af (_,_,initial_state,_,_) -> initial_state
+;;
+
+(* get_alphabet_symbol : draws conjunto of symbols from an automata *)
+let get_alphabet_symbol (autom:Auto.af) = match autom with
+  | Auto.Af (_,symbol,_,_,_) -> symbol
+;;
+
+(* get_list_states : draws conjunto of states and returns it as a list *)
+let get_list_states automata = Conj.list_of_conjunto (get_states automata);;
+
+(* get_list_transitions : draws conjunto of transitions and returns it as a list *)
+let get_list_transitions automata = Conj.list_of_conjunto (get_arcs automata);;
+
+(******************************************************************************)
+(*********************************** es_afne **********************************)
+(******************************************************************************)
+let es_afne automata = 
+  let arcs = get_arcs automata in (* get conjunto of all arcs *)
+  let rec es_afne = function (* for each arc... *)
+    | Conj.Conjunto [] -> false
+    | Conj.Conjunto (h::t) -> 
+      let symbol = get_arc_symbol h in (* get its symbol an check if its epsilon *)
+      if Conj.pertenece (Auto.Terminal "") (Conj.Conjunto [symbol]) then true
+      else es_afne (Conj.Conjunto t)
+  in es_afne arcs
+;;
+
+(* es_afne au1;; *)
+(* es_afne au2;; *)
+(* es_afne au3;; *)
+
+
+(******************************************************************************)
+(********************************** es_afn ************************************)
+(******************************************************************************)
+let rec check_afn_conditions t = function
+  | Conj.Conjunto [] -> false
+  | Conj.Conjunto (p::q) -> 
+    get_arc_initial_node t = get_arc_initial_node p &&
+    get_arc_end_node t = get_arc_end_node p &&
+    get_arc_symbol t <> Auto.Terminal "" &&
+    get_arc_symbol p <> Auto.Terminal "" &&
+    get_arc_symbol t <> get_arc_symbol p ||
+    check_afn_conditions t (Conj.Conjunto q)
+;;
+
+let es_afn automata =
+  let arcs = get_arcs automata in (* get conjunto of all arcs *)
+  let rec es_afn (* set_arcs *) = function (* match set_arcs with *)
+    | Conj.Conjunto [] -> false
+    | Conj.Conjunto (h::t) -> 
+      let t = Conj.Conjunto t in check_afn_conditions h t || es_afn t
+  in es_afn arcs
+;;
+
+(* es_afn au1;; *)
+(* es_afn au2;; *)
+(* es_afn au3;; *)
+
+
+(******************************************************************************)
+(********************************** es_afd ************************************)
+(******************************************************************************)
+let es_afd automata = es_afn automata && not (es_afne automata);;
+
+(* es_afd au1;; *)
+(* es_afd au2;; *)
+(* es_afd au3;; *)
+
+
+(******************************************************************************)
+(********************************* es_conexo **********************************)
+(******************************************************************************)
+(* es_conexo must check three conditions:
+ * 1. All automata states are reachable from the initial one. 
+ * 2. There is one or more end states.
+ * 3. All alphabet symbols are used in one transition at least *)
+
+(* First condition *)
+let f automata = 
+  let states = get_list_states automata in (* all states *)
+  let transitions = get_list_transitions automata in (* all transitions *)
+  let rec g (ns,nt) (ks,kt) = match ns with
+    | [] -> true  
+    | h::t -> match nt with
+      | [] -> (* TODO if there is previous, travel to previous else false *)
+      | p::q -> (* TODO transitions not empty *)
+  in g (states, transitions) ([],[])
+;;
+
+(* Previous code that it's not going to be used again, i think.
+let build_pairs automata = 
+  let ini_state = get_initial_state automata in
+  let states = Conj.suprimir ini_state (get_states automata) in
+  let rec build_pairs = function
+    | Conj.Conjunto [] -> []
+    | Conj.Conjunto (h::t) -> (ini_state, h) :: build_pairs (Conj.Conjunto t)
+  in build_pairs states
+;;
+
+let is_compatible_trans state trans = state = (get_arc_initial_node trans);;
+(* is_compatible_trans q0 a1;; *) (* must be true with au1 *)
+(* is_compatible_trans q0 a2;; *) (* must be false with au1 *)
+
+let check_pair pair transition = match transition with
+  | [] -> (* if not reached -> false || get not not taken transition *)
+  | h::t -> 
+
+let all_states_reachable automata =
+  let pairs = build_pairs automata in (* all posible pairs *)
+  let trans = get_arcs automata in (* all arcs in an automata *)
+  let rec all_states_reachable = function (* check pair one by one *)
+    | [] -> true
+    | h::t -> check_pair trans h && all_states_reachable t
+  in all_states_reachable pairs
+;;
+*)
+
+(* Second condition *)
+let end_state_has_elements automata = 
+  let end_node = get_end_state automata
+  in not (Conj.es_vacio end_node)
+;;
+
+(* Third condition *)
+(* build_conjunto_arc_sym : builds conjunto of arc symbols *)
+let rec build_conjunto_arc_sym conj_syms = function
+  | Conj.Conjunto [] -> conj_syms
+  | Conj.Conjunto (h::t) -> 
+    build_conjunto_arc_sym (Conj.agregar (get_arc_symbol h) conj_syms) (Conj.Conjunto t)
+;;
+
+let all_symbols_are_used automata = 
+  let conj_syms = build_conjunto_arc_sym (Conj.conjunto_vacio) (get_arcs automata) in
+  let sigma = get_alphabet_symbol automata in
+    Conj.incluido sigma conj_syms
+;;
+
+let es_conexo automata = 
+  (* TODO first condition *)
+  end_state_has_elements automata &&
+  all_symbols_are_used automata
+;;
