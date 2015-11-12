d0e30b23b90cde1721753709cdb80b4e4038950c
Heidy Mabel Izaguirre <hmiacatracha@gmail.com>
2015-11-12 11:21:01 +0100

    Añadido los cambios al modelo

--- MeteoApp/app/src/main/java/udc/es/meteoapp/model/Model.java
+++ MeteoApp/app/src/main/java/udc/es/meteoapp/model/Model.java
@@ -1,19 +1,23 @@
 package udc.es.meteoapp.model;
 
 
+import android.os.Handler;
+
 public class Model {
 
     FindLocality findLocality;
-    FindLocalityHandler findLocalityHandler;
+//    FindLocalityHandler findLocalityHandler;
+    Handler handler;
 
-    public Model() {
+    public Model(Handler handler) {
+        this.handler = handler;
 
     }
 
     public void findLocality(String localityName) {
-        findLocalityHandler = new FindLocalityHandler(); // TODO consider 'this' as argument
+        //findLocalityHandler = new FindLocalityHandler(); // TODO consider 'this' as argument
         findLocality =
-            new FindLocality("findLocality", findLocalityHandler, localityName, PlacesContent.apiKey);
+            new FindLocality("findLocality", handler, localityName, PlacesContent.apiKey);
         findLocality.start();
     }
 
