3ceed1214c169de55219ec918848aec926d7dffe
Raul <raulsg@gmail.com>
2016-10-05 19:31:46 +0200

    Añadidos comentarios finales y algunos cambios, falta traducir ;)

--- c/turingMachine.c
+++ c/turingMachine.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include "turingMachine.h"
 
+/* Crea una maquina de turing */
 machine create_MT(){
    machine mt;
+   // Memory allocation
    mt = (machine) malloc(sizeof(struct turingMachine));
    mt -> head = (head) malloc(sizeof(struct cell));
    if (mt == NULL) {
@@ -18,53 +20,40 @@ machine create_MT(){
    return mt;
 }
 
-
-void delete_MT(machine *mt, int lines){
-   while ((*mt)->head->right != NULL){
-      pcell tmp = ((*mt)->head)->right;
-      (*mt)->head->right = ((*mt)->head->right)->right;
-      free(tmp);
-   }
-   while ((*mt)->head->left != NULL){
-      pcell tmp = (*mt)->head->left;
-      (*mt)->head->left = ((*mt)->head->left)->left;
-      free(tmp);
-   }
-   
-   int i = 0;
-   for(i=0;i<lines;i++){
-      free((*mt)->conf[i]);
-   }
-   free((*mt)->conf);
-   free((*mt)->head);
-   free(*mt); 
+/* Coloca la cabeza en la celda mas a la izquierda de la cinta */
+void move_head_to_left(head *h){
+   if (*h!=NULL)
+      *h = (*h)->left;
 }
 
-void write_cell(head *c, char direction, tElement e){
+/* Escribe el caracter e en la cabeza y se mueve en la direccion direction */
+void write_cell(head *h, char direction, tElement e){
 
-   // The item is written into the current cell and the head moves in the direction given / Escribimos el elemento en la celda actual y nos movemos en funcion de la direccion dada
-   (*c) -> element = e;
+   // The item is written into the current cell / Escribimos el elemento en la celda actual
+   (*h) -> element = e;
 
    // Left direction
    if (direction == 'L'){ 
-
-      if ((*c) -> left == NULL){
+      // Si no hay celda izquierda se crea y se mueve el cabezal a la izquierda
+      if ((*h) -> left == NULL){
          pcell tmp = (pcell) malloc(sizeof(struct cell));
          if (tmp == NULL) {
             printf("Out of memory\n");
             exit(EXIT_FAILURE);
          }
-         tmp -> right = *c;
+         tmp -> right = *h;
          tmp -> left = NULL;
          tmp -> element = 'B';
-         (*c) -> left = tmp;
-         *c = tmp;
+         (*h) -> left = tmp;
+         *h = tmp;
+      // Si la celda izquierda existe, simplemente se mueve la cabeza a esa posicion
       } else {
-         *c = (*c) -> left;
+         *h = (*h) -> left;
       }
 
-   } else if (direction == 'R'){ // Right direction
-      if ((*c) -> right == NULL){
+   // Right direction, idem left
+   } else if (direction == 'R'){ 
+      if ((*h) -> right == NULL){
          pcell tmp = (pcell) malloc(sizeof(struct cell));
          if (tmp == NULL) {
             printf("Out of memory\n");
@@ -72,39 +61,40 @@ void write_cell(head *c, char direction, tElement e){
          }
 
          tmp -> right = NULL;
-         tmp -> left = *c;
+         tmp -> left = *h;
          tmp -> element = 'B';
-         (*c) -> right = tmp;
-         *c = tmp;
+         (*h) -> right = tmp;
+         *h = tmp;
       } else {
-         *c = (*c) -> right;
+         *h = (*h) -> right;
 
       }   
 
    }
 }
 
-void move_head_to_left(head *c){
-   if (*c!=NULL)
-      *c = (*c)->left;
-}
-
+/* Inicializa la maquina de turing */
 void initialize_MT(machine *mt, char **array, char *sequence){
    int i;
+   // Almacena el array de configuracion
    (*mt)->conf = array;
+   // Escribe la secuencia introducida por el usuario en la cinta
    for (i=0;i<strlen(sequence);i++){
       write_cell(&(*mt)->head,'R',sequence[i]);
    }
+   // Coloca la cabeza al principio de la cinta usando la funcion auxiliar move_head_to_left
    while ((*mt)->head->left != NULL){
       move_head_to_left(&(*mt)->head);
    }
 }
 
+/* Ejecuta la maquina de turing */
 void run_MT(machine *mt, int lines){
    char state;
    int steps = 0, running = 0, accept = 0;
    while(1){
-      if ( steps == 0){ // If it's the first step get initial state / Si es el primer paso obtenemos el estado inicial
+      // If it's the first step get initial state / Si es el primer paso obtenemos el estado inicial
+      if ( steps == 0){ 
          state = (*mt)->conf[0][0];       
       }
       
@@ -114,6 +104,7 @@ void run_MT(machine *mt, int lines){
       running = 0;
       int i;
       for (i=0;i<lines;i++){
+         // Si coincide, actualizamos el estado, escribimos en la cinta y nos movemos con la funcion auxiliar write_cell
          if (((*mt)->conf[i][0] == state) && ((*mt)->head->element == (*mt)->conf[i][2])){
             running = 1;
             state = (*mt)->conf[i][1];
@@ -142,6 +133,34 @@ void run_MT(machine *mt, int lines){
    printf("Steps: %i\n", steps);
 }
 
+/* Elimina la maquina de turing y libera la memoria*/
+void delete_MT(machine *mt, int lines){
+   /* Mientras exista una celda a la derecha de la cabeza, se libera su 
+   espacio. El puntero que apunta a la celda de la derecha pasa a apuntar
+   a la siguiente celda de su derecha. Así hasta liberar todas las celdas a la
+   derecha de la caebza.*/
+   while ((*mt)->head->right != NULL){
+      pcell tmp = ((*mt)->head)->right;
+      (*mt)->head->right = ((*mt)->head->right)->right;
+      free(tmp);
+   }
+   // Idem left
+   while ((*mt)->head->left != NULL){
+      pcell tmp = (*mt)->head->left;
+      (*mt)->head->left = ((*mt)->head->left)->left;
+      free(tmp);
+   }
+   // Se libera el espacio ocupado por el fichero de configuracion, la cabeza, y la maquina de turing
+   int i = 0;
+   for(i=0;i<lines;i++){
+      free((*mt)->conf[i]);
+   }
+   free((*mt)->conf);
+   free((*mt)->head);
+   free(*mt); 
+}
+
+/* Escribe los datos de la cinta en un fichero dada una ruta, si el fichero no existe lo crea */
 void mt_to_file(machine mt, char *file){
    // Open file in write mode
    FILE *pfile;
