9d3582cb495b57bbf890069793b6ee94378c3904
Diego Trabazo Sardón <dtrabazo@gmail.com>
2016-10-22 12:22:17 +0200

    Not correctly checking matrix dimensions fixed.

--- n2onedim.m
+++ n2onedim.m
@@ -1,6 +1,7 @@
 function outputImage = n2onedim (image)
-	dims = ndims (image);
-	if dims == 3
-		outputImage = rgb2gray (image);
-	end
-	outputImage = image;
+    dims = ndims (image);
+    if dims == 3
+        outputImage = rgb2gray (image);
+    elseif dims == 2
+        outputImage = image;
+    endif
