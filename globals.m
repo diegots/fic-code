a833e603deb1774d62d54f439b941a2db47fac61
Diego Trabazo Sardón <dtrabazo@gmail.com>
2016-11-04 19:50:12 +0100

    Fix value not declarated as string

--- globals.m
+++ globals.m
@@ -1,6 +1,6 @@
 % Global variables used by morphology operations
 global umbral = 0.69; % Threshold used to convert RGB or gray images to logical
-global strelsize = 5; % Size used in tophatFilter
+global strelsize = "5"; % Size used in tophatFilter
 
 % Prewitt kernel values
 global prewittKerX = [-1 -1 -1 0 0 0 1 1 1]; 
