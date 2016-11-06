ec2a50444f3b519b6a7b23096839403f01d2fe1c
Diego Trabazo Sardón <dtrabazo@gmail.com>
2016-11-06 17:18:34 +0100

    Add tests for edgeCannyAngle various cases

---
+++ edgeCannyAngleTest.m
@@ -0,0 +1,36 @@
+assert ( eq (edgeCannyAngle(0), 0) )
+assert ( eq (edgeCannyAngle(1), 0) )
+
+assert ( eq (edgeCannyAngle(44), 45) )
+assert ( eq (edgeCannyAngle(45), 45) )
+assert ( eq (edgeCannyAngle(46), 45) )
+
+assert ( eq (edgeCannyAngle(89), 90) )
+assert ( eq (edgeCannyAngle(90), 90) )
+assert ( eq (edgeCannyAngle(91), 90) )
+
+assert ( eq (edgeCannyAngle(134), 135) )
+assert ( eq (edgeCannyAngle(135), 135) )
+assert ( eq (edgeCannyAngle(136), 135) )
+
+assert ( eq (edgeCannyAngle(158), 0) )
+
+assert ( eq (edgeCannyAngle(179), 0) )
+assert ( eq (edgeCannyAngle(180), 0) )
+assert ( eq (edgeCannyAngle(181), 0) )
+
+assert ( eq (edgeCannyAngle(224), 45) )
+assert ( eq (edgeCannyAngle(225), 45) )
+assert ( eq (edgeCannyAngle(226), 45) )
+
+assert ( eq (edgeCannyAngle(269), 90) )
+assert ( eq (edgeCannyAngle(270), 90) )
+assert ( eq (edgeCannyAngle(271), 90) )
+
+assert ( eq (edgeCannyAngle(314), 135) )
+assert ( eq (edgeCannyAngle(315), 135) )
+assert ( eq (edgeCannyAngle(316), 135) )
+
+assert ( eq (edgeCannyAngle(359), 0) )
+assert ( eq (edgeCannyAngle(360), 0) )
+assert ( eq (edgeCannyAngle(361), 0) )
