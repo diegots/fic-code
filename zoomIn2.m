a0048a8942787f140f337e92466c87baadded0d0
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-06-10 14:22:04 +0200

    Fix calc_grow_vectors when scale was greater than one

--- zoomIn2.m
+++ zoomIn2.m
@@ -1,19 +1,21 @@
 % zoomIn2 applies Interpolation to an image. Implements Nearest Neighbor and
 % Bilinear algorithm
-% inputImage: greyescale image
+% inputImage: greyscale image
 % mode: either "neighbor" or bilinear
-% escale: factor applied to inputImage
-function outputImage = zoomIn2 ( inputImage , mode, escale )
+% scale: factor applied to inputImage
+function outputImage = zoomIn2 ( inputImage , mode, scale )
     
     if (exist(inputImage) != 2)
         printf('A path to a image is needed!\n\n');
         return
     endif
     if (exist('mode') != 1)
-        printf("What 'mode' should be used? Both neighbor and bilinear are supported\n\n");
+        msg = strcat ("What 'mode' should be used? Both neighbor and bilinear",
+            "are supported\n\n")
+        printf(msg);
         return
     endif
-    if (exist('escale') != 1)
+    if (exist('scale') != 1)
         printf('Please, provide desired scale factor\n\n');
         return
     endif
@@ -25,7 +27,7 @@ function outputImage = zoomIn2 ( inputImage , mode, escale )
         image = rgb2gray (image);
     endif
     
-    [rows cols] = size (image);
+    printf('Original image size: %s x %s px\n', int2str(cols), int2str(rows));
     
     %
     % Bilinear Interpolation
@@ -33,23 +35,26 @@ function outputImage = zoomIn2 ( inputImage , mode, escale )
     if strcmp (mode, 'bilinear')
         printf('Interpolating image by bilinear method\n');
         
-        new_rows = escale*rows;
-        new_cols = escale*cols;
-        p_rows = ( (1 : floor(new_rows/rows) : new_rows) ...
-            + (floor(new_rows/rows/2)) ) (1:rows);
-        p_cols = ( (1 : floor(new_cols/cols) : new_cols) ...
-            + (floor(new_cols/cols/2)) ) (1:cols);
-        outputImage = zeros (new_rows, new_cols);
-        outputImage(p_rows, p_cols) = image (1:rows, 1:cols);
-        outputImage (2:new_rows-1, 2:new_cols-1) ...
-            = uint8 (floor(
-                  outputImage (1:new_rows-2, 1:new_cols-2)   ...
-                + outputImage (1:new_rows-2,   3:new_cols)   ...
-                + outputImage (3:new_rows,     1:new_cols-2) ...
-                + outputImage (3:new_rows,     3:new_cols  )  )/4 );
+        new_cols = floor( scale*cols );
+        new_rows = floor( scale*rows );
 
-        show_results (outputImage) 
-        outputImage = uint8 (outputImage);
+        col_repetition = calc_grow_vectors (cols);
+
+%        p_rows = ( (1 : floor(new_rows/rows) : new_rows) ...
+%            + (floor(new_rows/rows/2)) ) (1:rows);
+%        p_cols = ( (1 : floor(new_cols/cols) : new_cols) ...
+%            + (floor(new_cols/cols/2)) ) (1:cols);
+%        outputImage = zeros (new_rows, new_cols);
+%        outputImage(p_rows, p_cols) = image (1:rows, 1:cols);
+%        outputImage (2:new_rows-1, 2:new_cols-1) ...
+%            = uint8 (floor(
+%                  outputImage (1:new_rows-2, 1:new_cols-2)   ...
+%                + outputImage (1:new_rows-2,   3:new_cols)   ...
+%                + outputImage (3:new_rows,     1:new_cols-2) ...
+%                + outputImage (3:new_rows,     3:new_cols  )  )/4 );
+%
+%        show_results (outputImage) 
+%        outputImage = uint8 (outputImage);
 
     %
     % Neighbor Interpolation
@@ -57,6 +62,7 @@ function outputImage = zoomIn2 ( inputImage , mode, escale )
     elseif strcmp (mode, 'neighbor')
         printf('Interpolating image by neighbor method\n');
         
+        [rows cols] = size (image);
         col_repetitions = calc_grow_vectors(cols); % get cols
         positions = 1 : length ( image'(:) );
         repetitions = repmat (col_repetitions(:), 1, rows)(:)';
@@ -82,51 +88,55 @@ function outputImage = zoomIn2 ( inputImage , mode, escale )
     function res = show_results (res_image)
         outputImageStr = "new-image.png";
 
-        [rows, cols] = size (res_image);
-        printf("New image size: %dx%d px\n", rows, cols);
+        [cols, rows] = size (res_image);
+        printf("New image size: %dx%d px\n", cols, rows);
         imshow (res_image)
-        msg = strcat ("New size: ", int2str(rows), " x ", int2str(cols), " px");
+        msg = strcat ("New size: ", int2str(cols), " x ", int2str(rows), " px");
         text (30, cols+30, msg, "fontsize", 12)
         imwrite (res_image, outputImageStr)
         printf("Written output image to '%s'\n", outputImageStr);
         
     endfunction % end show_results
     
-    % Helper function: calculates grow vectors
+    % Helper function: calculates how many points should be put between two 
+    % points in the original matrix. Example: with 10 px in the original
+    % image and a scale of 1.5 in between each point must be,
+    % 2 1 1 2 1 1 2 1 1 2 points
     function v = calc_grow_vectors (vector)
-        new_vector = floor ( escale * vector );
+        new_vector = floor ( scale * vector );
         vec_grow = zeros (1, vector);
         
-        div = floor (new_vector / vector);
-        modulus = mod(new_vector, vector);
+        div = floor (new_vector / vector );
+        modulo = mod (new_vector, vector );
         
         odd_range = 1 : 2 : vector;
         even_range = 2 : 2 : vector;
         
+        % Base values. This is what can be distributed equaly
         vec_grow(odd_range) = 1 * div;
         vec_grow(even_range) = 1 * div;
 
         % add modulus part if modulus is not 0
-        if modulus >= 1
+        if modulo >= 1
             
-            if escale >= 1 % Increasing image
-                p = 1 : (new_vector / modulus) : vector;
+            if scale >= 1 % Increasing image
+                p = linspace (1, vector, modulo);
                 p = floor(p);
             else % Reducing image
-                p = 1 : (1 / escale) : vector;
+                p = 1 : (1 / scale) : vector;
                 p = floor(p);
             endif
             
-            odd_extra = p (mod(p,2)!=0);
-            even_extra = p (mod(p,2)==0);
+            odd_extra = p ( mod(p,2) != 0 ); % Positions that will host extra item
+            even_extra = p ( mod(p,2) == 0 );
             
-            [r c] = size (odd_extra);
-            if r >= 1 && c >= 1
+            [c r] = size (odd_extra);
+            if c >= 1 && r >= 1
                 vec_grow(odd_extra) += 1;
             endif
             
-            [r c] = size (even_extra);
-            if r >= 1 && c >= 1
+            [c r] = size (even_extra);
+            if c >= 1 && r >= 1
                 vec_grow(even_extra) += 1;
             endif
         endif
