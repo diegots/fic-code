b0ab944eb5cc21477acb16dbc0d78cfb2b764210
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-06-14 10:06:32 +0200

    Thresholding sets px of value 1, not 255

--- uThresholding.m
+++ uThresholding.m
@@ -1,7 +1,7 @@
 function res = uThresholding (image, threshold)
 
     image(image<threshold)=0;
-    image(image>=threshold)=255;
+    image(image>=threshold)=1;
     res = image;
 
 endfunction % end script
