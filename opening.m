3ea06b9e6644d06bdef00bd154a0368bfbaaab94
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-06-14 19:17:16 +0200

    Add opening implementation

---
+++ opening.m
@@ -0,0 +1,19 @@
+function outputImage = opening ( inputImage , strElType, strElSize )
+
+    THRESHOLD = 125;
+
+    image = uReadImage (inputImage);
+    image = uThresholding (image, THRESHOLD);
+    kernel = uCreateKernel (strElType, strElSize);
+    outputImage = uConvolution (image, kernel, 'erode');
+    outputImage = uConvolution (outputImage, kernel, 'dilate');
+
+    figure (1)
+    imshow (image)
+    text (1, -14, "Imagen original", "fontsize", 20)
+
+    figure (2)
+    imshow (outputImage)
+    text (1, -14, "Imagen nueva", "fontsize", 20)
+
+endfunction
