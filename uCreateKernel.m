69632f2410bdec127cd920843752dca982049d8b
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-06-14 10:05:34 +0200

    Create all kernels: cross, square, linev, lineh

--- uCreateKernel.m
+++ uCreateKernel.m
@@ -1,15 +1,18 @@
 function res = uCreateKernel (type, filterSize)
 
+    % Median kernel is just an ones's square with the desired size
     if (strcmp(type, 'median') )
         res = ones (filterSize, filterSize);
 
     elseif (strcmp(type, 'cross') )
+        res = zeros (filterSize,filterSize);
+        reshape ([1:filterSize*filterSize],filterSize,filterSize);
         full = reshape ([1:filterSize*filterSize],filterSize,filterSize);
         point = ceil (filterSize / 2);
         v1 = full (point, :)';
         v2 = full (:, point);
-        res = cat (1, v1, v2);
-        res = sort (unique (res));
+        res (v1) = 1;
+        res (v2) = 1;
 
     elseif (strcmp(type, 'square') )
         res = reshape ([1:filterSize*filterSize],filterSize,filterSize)(:);
