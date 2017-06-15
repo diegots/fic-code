ce403c28647f6de3ffbf36d8d0af3bbfa2a80aeb
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-06-15 19:43:13 +0200

    Add uMain, useful to test implementations

---
+++ uMain.m
@@ -0,0 +1,45 @@
+% Code for calling and showing all implemented functions
+
+% uConvolution
+%mat = round (100 * rand (7,7) );
+%ker = uCreateKernel ('square', 3);
+%ker = uCreateKernel ('linev', 3);
+%uConvolution (mat, ker, 'erode');
+
+THRESHOLD = 125;
+WHITE = 255;
+
+% images
+lenna     = 'images/lenna512.bmp';
+building  = 'images/building.jpg';
+landscape = 'images/landscape.jpg';
+
+% histShrink
+%new = histShrink(lenna, 50, 170);
+
+% zoomIn2
+%new = zoomIn2 (building, 'neighbor' , 2.3);
+%new = zoomIn2 (building, 'bilinear' , 2.3);
+
+% medianFilter
+%new = medianFilter (landscape, 7);
+
+% erode
+%new = erode (landscape, 'square', 3);
+%new = erode (building, 'linev', 5);
+%landscape = uThresholding (landscape, THRESHOLD);
+
+% dilate
+new = dilate (landscape, 'square', 5);
+landscape = uThresholding (landscape, THRESHOLD);
+
+% opening
+%new = opening (building, 'cross', 5);
+%building = uThresholding (building, THRESHOLD);
+
+% closing
+%new = opening (building, 'cross', 5);
+%building = uThresholding (building, THRESHOLD);
+
+% show or write
+uShowWriteOut (landscape, new, ''); % by default show
