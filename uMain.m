53c1d2bd7f9bb1997f173f716e31f87888a43e5d
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-07-04 23:43:25 +0200

    Adds Harris corners calls with somewhat valid thresholds

--- uMain.m
+++ uMain.m
@@ -13,12 +13,15 @@ WHITE = 255;
 % images
 lenna     = 'images/lenna512.bmp';
 %lenna     = 'images/lenna128.bmp';
+%lenna     = 'images/lenna24.bmp';
+%lenna     = 'images/lenna12.bmp';
+%lenna     = 'images/lenna10.bmp';
 building  = 'images/building.jpg';
 landscape = 'images/landscape.jpg';
 
-originalImg = uReadImage (lenna);
+%originalImg = uReadImage (lenna);
 %originalImg = uReadImage (building);
-%originalImg = uReadImage (landscape);
+originalImg = uReadImage (landscape);
 
 % histShrink
 %new = histShrink(originalImg, 60, 230);
@@ -75,10 +78,14 @@ originalImg = uReadImage (lenna);
 %new = conv2(originalImg, gx, 'same')
 %new = uConvolutionGeneral (originalImg, gy)
 
-% Harris corner detector
-new = cornerHarris (originalImg, 3, 1.0e+10);
-%new = cornerHarris (originalImg, 3, 0.8);
-
 % show or write
 %uShowWriteOut (originalImg, new, ''); % by default show images
 %uShowWriteOut (originalImg, new, 'write'); % by default, '', display result
+
+% Harris corner detector 
+% It plots it's own data
+%new = cornerHarris (originalImg, 3, 1.0e+10); % umbral for Lenna
+%new = cornerHarris (originalImg, 3, 1.0e+12); % umbral for building
+new = cornerHarris (originalImg, 3, 1.0e+12); % umbral for building
+
+
