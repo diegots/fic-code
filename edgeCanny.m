7684dcba6c4a826e7259d793235b64fa203a2b97
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-07-06 15:31:53 +0200

    Canny detector w/o threshold hysteresis

---
+++ edgeCanny.m
@@ -0,0 +1,117 @@
+% edgeCanny
+function outputImage = edgeCanny (inputImage, sgm, tlow, thigh)
+
+
+    % Gaussian filter
+    mu = 0;
+    size_filtro = [3, 3]; % filter 3 x 3
+    s1 = size_filtro (1)-1;
+    s2 = size_filtro (2)-1;
+    [r, c] = meshgrid(0:s2, 0:s1);
+    r = r-s2/2;
+    c = c-s1/2;
+    gauss = exp( -( r.^2 + c.^2 ) / (2*sgm^2) ); % el centro es 1
+    filter = gauss / sum (gauss (:)); % la matrix suma 1
+
+    outputImage = uConvolutionGeneral (inputImage, filter);
+
+    %filter = fspecial ('gaussian', 3, 0.5);
+    %outputImage = conv2 (inputImage, filter, 'same');
+
+    % Use sobel as border detector
+    border_detector_size = 3;
+    gx = fspecial ('sobel', border_detector_size);
+    gy = fspecial ('sobel', border_detector_size)';
+
+    ix = uConvolutionGeneral (inputImage, gx);
+    iy = uConvolutionGeneral (inputImage, gy);
+
+    [rows, cols] = size (inputImage);
+    indexes = reshape(1:rows*cols,rows,cols);
+    bds = border_detector_size / 2; 
+    centers = indexes (ceil(bds/2):rows-(floor(bds/2)), ceil(bds/2):cols-(floor(bds/2)));
+
+    coners_ = find (ix);
+    ix_borders = centers (coners_); % all X borders
+
+    % Non-max supression
+    a = atan (iy./ix);
+    nan_pos = isnan(a); % atan calculus produces some NaN values on the matrix borders
+    a(nan_pos) = 0; % removes those NaN values
+    Eo = mod((180 * a / pi) + 360, 180); % orientation matrix
+    Em = sqrt (ix.^2 .+ iy.^2); % magnitude matrix
+
+    d1 = (Eo>0 & Eo<45);    % 0º to 45º 
+    d2 = (Eo>=45 & Eo<90);  % 45º to 90º 
+    d3 = (Eo>=90 & Eo<135); % 90º to 135º 
+    d4 = (Eo>=135);         % 135º to 180º
+
+    cd1 = intersect (ix_borders, find(d1));
+    cd1( Em(cd1)<Em(cd1+1) ) = [];
+    cd1( Em(cd1)<Em(cd1-1) ) = [];
+
+    cd2 = intersect (ix_borders, find(d2));
+    cd2( Em(cd2)<Em(cd2-rows-1) ) = [];
+    cd2( Em(cd2)<Em(cd2+rows+1) ) = [];
+
+    cd3 = intersect (ix_borders, find(d3));
+    cd3( Em(cd3)<Em(cd3-rows) ) = [];
+    cd3( Em(cd3)<Em(cd3+rows) ) = [];
+
+    cd4 = intersect (ix_borders, find(d4));
+    cd4( Em(cd4)<Em(cd4-rows+1) ) = [];
+    cd4( Em(cd4)<Em(cd4+rows-1) ) = [];
+
+    ix_borders = union (cd1, cd2);
+    ix_borders = union (ix_borders, cd3);
+    ix_borders = union (ix_borders, cd4);
+
+
+    % 
+
+    % Show results
+    [y,x] = get_relative_indexes (rows,cols,ix_borders);
+    printf('Detected points x gradient: %s\n', num2str(numel(ix_borders)));
+    %figure (9,'visible','off')
+    figure (8);
+    imshow(inputImage)
+    hold on
+    %imshow(255*ones(rows,cols))
+    plot(x,y,"color","r","*");
+    hold off
+    %print (gcf(), "outputImage.png", '-dpng')
+
+    outputImage = inputImage;
+
+endfunction
+% END edgeCanny
+
+
+%
+%
+%
+function [v0,v1] = drawCircle (x, y, r)
+    
+    t = linspace(0,2*pi,30)';
+    cos_x = r * cos(t);
+    sin_y = r * sin(t);
+
+    e = numel (x); % let's repeat t
+    cos_x_rep = repmat (cos_x,1,e);
+    sin_y_rep = repmat (sin_y,1,e);
+
+    v0 = cos_x_rep + x';
+    v1 = sin_y_rep + y';
+    
+endfunction
+
+
+%
+%
+%
+function [y,x] = get_relative_indexes (rows,cols,points)
+    % Get corners coordinates (x,y)
+    p = zeros (rows,cols);
+    p(points) = 1;
+    [y,x] = find(p);
+endfunction
