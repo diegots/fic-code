56eb079d9751afbe2a5882f7951d26ab1d37ee38
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-07-06 15:32:46 +0200

    Changes to operate with edge Canny detector

--- uMain.m
+++ uMain.m
@@ -11,17 +11,17 @@ THRESHOLD = 125;
 WHITE = 255;
 
 % images
-lenna     = 'images/lenna512.bmp';
-%lenna     = 'images/lenna128.bmp';
+%lenna     = 'images/lenna512.bmp';
+lenna     = 'images/lenna128.bmp';
 %lenna     = 'images/lenna24.bmp';
 %lenna     = 'images/lenna12.bmp';
 %lenna     = 'images/lenna10.bmp';
 building  = 'images/building.jpg';
 landscape = 'images/landscape.jpg';
 
-%originalImg = uReadImage (lenna);
+originalImg = uReadImage (lenna);
 %originalImg = uReadImage (building);
-originalImg = uReadImage (landscape);
+%originalImg = uReadImage (landscape);
 
 % histShrink
 %new = histShrink(originalImg, 60, 230);
@@ -80,12 +80,14 @@ originalImg = uReadImage (landscape);
 
 % show or write
 %uShowWriteOut (originalImg, new, ''); % by default show images
-%uShowWriteOut (originalImg, new, 'write'); % by default, '', display result
+%uShowWriteOut (originalImg, new, 'write'); % write image to a file
 
 % Harris corner detector 
 % It plots it's own data
 %new = cornerHarris (originalImg, 3, 1.0e+10); % umbral for Lenna
 %new = cornerHarris (originalImg, 3, 1.0e+12); % umbral for building
-new = cornerHarris (originalImg, 3, 1.0e+12); % umbral for building
+%new = cornerHarris (originalImg, 3, 1.0e+12); % umbral for building
 
+% Edge Canny
+new = edgeCanny (originalImg, 0.5, 1, 4); % img, sigma, tLow, tHigh
 
