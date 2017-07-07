8c0e8a900027f5496e30c73c0eb3c194a7f2334b
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-07-07 10:06:51 +0200

    Reverse kernel matrix as described in documentation

--- uConvolutionGeneral.m
+++ uConvolutionGeneral.m
@@ -4,6 +4,8 @@ function res = uConvolutionGeneral (mat, kernel)
     [r, c] = size (mat);
     [kr, kc] = size (kernel);
 
+    kernel = reshape( (kernel(:))(end:-1:1), kr, kc)
+
     indexes = reshape(1:r*c,r,c);
     firstMatConv = indexes (1:kr,1:kc)(:);
     centers = indexes (ceil(kr/2):r-(floor(kr/2)), ceil(kc/2):c-(floor(kc/2)));
@@ -20,8 +22,10 @@ function res = uConvolutionGeneral (mat, kernel)
 
     % conv2 returns the same matrix as res calculated here but if kernel has 
     % negative values such as a Sobel kernel, values are multiplied by -1
-    if ( eq(sum(kernel(:)), 0) )
-        res = res * (-1);
-    endif
+%    if ( eq(sum(kernel(:)), 0) )
+%        res = res * (-1);
+%    endif
+    % This code is not really needed. It's just neccessary to reverse the 
+    % filter like was seen in the lecture.
     
 endfunction % end script
