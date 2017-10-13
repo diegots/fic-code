360d21fc7a18c02265f48f8ad6500bbea275d408
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-10-13 11:45:17 +0200

    Adds histAdapt implementation

---
+++ histAdapt.m
@@ -0,0 +1,44 @@
+function outputImage = histAdapt (inputImage, minValue, maxValue)
+
+  % Normalize entre el rango 0-1
+  function out =  normalize (x, min, max)
+    out = (x(1:end)/(max-min)) - (min/(max-min));
+  end
+
+  % Cambia la entrada del rango 0-1 al nuevo
+  function out = expand (x, min, max)
+    out = ((max-min) * x(1:end)) + ((min / (max - min)) * (max-min));
+  end
+
+  image = double(uReadImage(inputImage));
+
+  t = min (image);
+  oldMinValue = min (t) % mínima intensidad en la imagen
+  t = max (image);
+  oldMaxValue = max (t) % maxima intensidad en la imagen
+
+  disp(sprintf('Transformando el histograma de %u-%u a %u-%u', ...
+    oldMinValue, oldMaxValue, minValue, maxValue))
+
+  %p = 245
+  %p_norm = (p/(oldMaxValue-oldMinValue)) - (oldMinValue/(oldMaxValue-oldMinValue))
+  %p_final = ((maxValue-minValue) * p_norm) + (minValue / (maxValue-minValue))*(maxValue-minValue)
+
+  % Inicializa la variable de salida
+  [a,b] = size (image);
+  outputImage = double (zeros (a,b));
+
+  % Normaliza los valores de la imagen de entrada entre 0-1
+  image_norm = normalize(image, oldMinValue, oldMaxValue);
+  outputImage = expand(image_norm, minValue, maxValue);
+
+  %round(linspace (10,110,256))
+
+  image = uint8 (image);
+  outputImage = reshape( uint8 (outputImage), a, b);
+
+  figure(1)
+  imhist(image);
+  figure(2)
+  imhist(outputImage);
+end
