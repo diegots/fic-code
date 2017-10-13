0312ed305a3eaa973cef85259c22b2edaa856feb
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-10-13 11:44:35 +0200

    Adapts uReadImage to work with Matlab

--- uReadImage.m
+++ uReadImage.m
@@ -1,11 +1,11 @@
 
-function res = uReadImage (inputImage)
-
+function res = readImage (inputImage)
+% Ejemplo de carga de imagen:
+% uReadImage('images\lenna512.bmp')
     image = imread (inputImage);
     if (ndims (image) == 3)
         image = rgb2gray (image);
     end
 
     res = uint8 (image);
-endfunction
-
+end
