4438d4b05101f7879c0fd89e39d6ab30793dfbdb
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-10-29 17:42:25 +0100

    Añade fichero para el filtro de medianas

---
+++ medianFilter2D.m
@@ -0,0 +1,9 @@
+% Implementar el filtro de orden de medianas. La función permitirá establecer el
+% tama˜no del filtro.
+function outputImage = medianFilter2D (inputImage, filterSize)
+	disp(sprintf('[medianFilter2D] inicio'))
+	
+	
+	
+	
+end
