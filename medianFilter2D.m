0748ae96fe17c4293621135cb45979fe409ccb1e
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-10-29 18:53:49 +0100

    Filtro de medianas
    Se modifica la operación de convolución para poder ser utilizada también
    por el filtro de medianas

--- medianFilter2D.m
+++ medianFilter2D.m
@@ -1,9 +1,11 @@
 % Implementar el filtro de orden de medianas. La función permitirá establecer el
 % tama˜no del filtro.
 function outputImage = medianFilter2D (inputImage, filterSize)
-	disp(sprintf('[medianFilter2D] inicio'))
+	disp(sprintf('[medianFilter2D] Llamando a uConvolve con tamaño %d de filtro', filterSize))
 	
+	% Se genera una matriz de unos del tama˜no filterSize
+	kernel = ones (filterSize);
 	
-	
+	outputImage = uConvolve (inputImage, kernel, 'median');
 	
 end
