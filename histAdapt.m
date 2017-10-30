0f1b110555bbae1bcc22a507d7208bdd44c5ada5
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-10-30 23:00:32 +0100

    Simplifica la función histAdapt

--- histAdapt.m
+++ histAdapt.m
@@ -2,8 +2,6 @@ function outputImage = histAdapt (inputImage, minValue, maxValue)
 % histAdapt adapta el histograma de la imagen pasada en el parámetro inputImage
 % al nuevo rango comprendido entre minValue y maxValue
 
-  MAX_INTENSITY = 255; %  se consideran imágenes de 8 bits/pixel
-
   % Cambia la entrada del rango 0-1 al nuevo
   function out = expand (x, min, max)
   % expand mueve el rango de valores normalizados de la matriz de entrada a un
@@ -16,23 +14,7 @@ function outputImage = histAdapt (inputImage, minValue, maxValue)
   % Obtiene máximo y mínimo valor de intensidad de la imagen
   oldMinValue = min (min (inputImage));
   oldMaxValue = max (max (inputImage));
-
-  % Si la imagen no está normalizada, se normaliza
-  if (oldMaxValue > 1)
-    disp('Normalizando la imagen entre 0-1.')
-    inputImage = uNormalize(image, oldMinValue, oldMaxValue);
-
-    oldMinValue = oldMinValue / MAX_INTENSITY;
-    oldMaxValue = oldMaxValue / MAX_INTENSITY;
-    minValue = minValue / MAX_INTENSITY;
-    maxValue = maxValue / MAX_INTENSITY;
-  end
-
-  if (maxValue > 1)
-    minValue = minValue / MAX_INTENSITY;
-    maxValue = maxValue / MAX_INTENSITY;
-  end
-
+  
   disp(sprintf('Transformando el histograma de %4.2f-%4.2f a %4.2f-%4.2f', ...
   oldMinValue, oldMaxValue, minValue, maxValue))
 
