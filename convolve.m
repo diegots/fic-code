2f1f4d72b02e51e5746bca647bc57fa0cbeb85e8
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-10-30 18:15:46 +0100

    Añade llamada a la convolución de MATLAB

--- convolve.m
+++ convolve.m
@@ -1,4 +1,7 @@
 function outputImage = convolve (inputImage , kernel)
 	disp('[convolve] Llamando a uConvolve')
 	outputImage = uConvolve (inputImage, kernel, '2dconvo');
+	
+	% Implementación de convolución de MATLAB
+	%outputImage = conv2(inputImage, kernel, 'same');
 end
