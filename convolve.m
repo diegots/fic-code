039af2a2be5607eebb73650baacc6fb4ad73bfb7
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-11-01 22:17:19 +0100

    Añade comentarios

--- convolve.m
+++ convolve.m
@@ -1,4 +1,9 @@
-
+% Convolución 2D
+% shape = 'full' | 'same'
+% shape puede ser full, para realizar la convolución también por los bordes y
+% devolviendo una imagen expandida.
+% shame realiza la convolución sin expandir la imagen. Las zonas por las que no
+% se puede pasar el kernel se dejan a cero
 function outputImage = convolve (inputImage , kernel, shape)
 	disp('[convolve] Llamando a uConvolve')
 	outputImage = uConvolve (inputImage, kernel, shape, '2dconvo');
