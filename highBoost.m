9b05d40c9a8b9a1d8809305679cddb27b8f0a76b
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-11-03 22:04:33 +0100

    Completa High Boost

--- highBoost.m
+++ highBoost.m
@@ -1,9 +1,13 @@
 function outputImage = highBoost (inputImage, A, method, parameter)
-
+	% https://www.youtube.com/watch?v=Qcl5g1UtEv4
+	
 	[r,c] = size (inputImage);
 	outputImage = zeros(r,c);
 	
-	disp(sprintf('[highBoost] Calculando High Boost con parámetro A=%d', A))
+	disp(sprintf('[highBoost] Calculando High Boost con parámetro A =%5.2f', A))
+	disp('                    High Boost es una generalización de Unsharp Mask.')
+	disp('                    Unsharp Mask calcula g(x,y) = f(x,y)-ver_smooth(x,y)')
+	disp('                    High Boost calcula g(x,y) = A * f(x,y) - ver_smooth(x,y) con A>=0')
 
 	imagen_suavizada = zeros(r,c);	
 	if (strcmp (method, 'gaussian'))
@@ -26,14 +30,17 @@ function outputImage = highBoost (inputImage, A, method, parameter)
 	[a b] = size(imagen_suavizada);
 	disp(sprintf('[highBoost] Tamaño imagen suavizada %dx%d', a, b))
 	
-	
+	% Encogemos la imagen para devolverla con el mismo tamaño que la original
 	imagen_suavizada = uExtendShrink(imagen_suavizada, kernel, 'shrink');
 	
 	[a b] = size(imagen_suavizada);
 	disp(sprintf('[highBoost] Tamaño imagen suavizada con shrink: %dx%d', a, b))
 	
 	o = (A .* inputImage) - imagen_suavizada;
-	outputImage = histAdapt(o, 0,1);
+	
+	% Después de hacer el producto con A, los valores resultates se salen del 
+	% rengo de trabajo 0-1, así que normalizamos.
+	outputImage = histAdapt(o, 0, 1);
 	%outputImage = imadjust(o, stretchlim(o),[0 1]);
 	 
 end
