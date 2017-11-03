f62b6e450a60cd26fa388914901636ec4e994c0e
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-11-03 22:02:48 +0100

    Completa medianFilter2D

--- medianFilter2D.m
+++ medianFilter2D.m
@@ -1,15 +1,20 @@
 % Implementar el filtro de orden de medianas. La función permitirá establecer el
 % tamaño del filtro.
 function outputImage = medianFilter2D (inputImage, filterSize)
-	disp(sprintf('[medianFilter2D] Llamando a uConvolve con tamaÃ±o %d de filtro', ...
+	disp(sprintf('[medianFilter2D] Llamando a uConvolve con tamaño de filtro %d', ...
 		filterSize))
 	
 	% Se genera una matriz de unos del tamaño filterSize
 	kernel = ones (filterSize);
 	
-	outputImage = uConvolve (inputImage, kernel, 'full', 'median');
+	shape = 'full';
+	%shape = 'same';
+	
+	outputImage = uConvolve (inputImage, kernel, shape, 'median');
 	
 	% Implementación de MATLAB
-	%outputImage = medfilt2(inputImage, [filterSize filterSize]) ;
+	% No funciona correctamente porque el resultado no es la imagen apliada
+	% como en el caso de una convolución full
+	%outputImage = medfilt2(inputImage, [filterSize filterSize],'zeros');
 	
 end
