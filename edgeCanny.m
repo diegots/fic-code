ed3a8b1a9b754e64582cd0035c2fb78d0d12dad8
Diego Trabazo Sardón <dtrabazo@gmail.com>
2017-11-08 10:41:29 +0100

    Canny con histéresis

--- edgeCanny.m
+++ edgeCanny.m
@@ -1,6 +1,6 @@
-% Implementar el detector de bordes de Canny mediante una funci√≥n que permita 
-% especificar el valor de œÉ en el suavizado Gaussiano y los umbrales del proceso
-% de hist√©resis
+% Implementar el detector de bordes de Canny mediante una funciÛn que permita 
+% especificar el valor de sigma en el suavizado Gaussiano y los umbrales del proceso
+% de histÈresis
 function outputImage = edgeCanny (inputImage, sigma, tlow, thigh)
 
 	function [sig restantes] = siguiente(actual, direccion, porVisitar, r, c)
@@ -29,7 +29,7 @@ function outputImage = edgeCanny (inputImage, sigma, tlow, thigh)
 			end
 		end
 		
-		% √öltima comprobaci√≥n: el nuevo punto debe estar entre los porVisitar
+		% ⁄ltima comprobaciÛn: el nuevo punto debe estar entre los porVisitar
 		if (find(ismember(porVisitar,sig)))
 			;
 		else
@@ -72,8 +72,8 @@ function outputImage = edgeCanny (inputImage, sigma, tlow, thigh)
 		v1 = 0;
 		v2 = 0;
 	
-		% obtiene los subindices para un punto pasado como √≠ndice linea en una
-		% matriz de tama√±o r,c
+		% obtiene los subindices para un punto pasado como Ìndice linea en una
+		% matriz de tamaÒo r,c
 		[r_ c_] = ind2sub ([r c], coord);
 		
 		if (strcmp (direccion, 'H'))
@@ -112,13 +112,21 @@ function outputImage = edgeCanny (inputImage, sigma, tlow, thigh)
 				v =  [magnitud(elem) magnitud(v1) magnitud(v2)]; % las 3 magnitudes
 				
 				% indice de la mayor de las magnitudes dentro del vector v: 1, 2 o 3
-				max_index = find (v == max(v)); % encuentra el √≠ndice del mayor en el vector v
+				max_index = find (v == max(v)); % encuentra el Ìndice del mayor en el vector v
 				
 				% Si el √≠ndice obtenido se corresponde con 1, que es el del punto actual,
-				% enconces la comparaci√≥n es true y vale 1, en caso contrario 0.
-				% Por √∫ltimo multiplica ese 0 o 1 por el valor de la magnitud del
+				% enconces la comparaciÛn es true y vale 1, en caso contrario 0.
+				% Por ˙ltimo multiplica ese 0 o 1 por el valor de la magnitud del
 				% elemento actual, que es lo que se almacena
-				bor(elem) = (1 == max_index) * magnitud(elem); 
+				if (numel (max_index) == 1)
+					bor(elem) = (1 == max_index) * magnitud(elem); 
+				else
+					% Ojo puede haber 2 o tres elementos magnitudes iguales
+					% detectamos si elem est·n entre ellas (caso de 2)
+					if (ismember(max_index, 1))
+						bor(elem) = magnitud(elem); 
+					end
+				end
 			end
 		end
 	end
@@ -130,16 +138,17 @@ function outputImage = edgeCanny (inputImage, sigma, tlow, thigh)
 	[r,c] = size (inputImage);
 	Eo = zeros (r,c); % Inicializa Eo
 	Em = zeros (r,c); % Inicializa Em
+	output = zeros (r,c);
 	outputImage = zeros (r,c); % matriz de bordes resultante
 	
 	% Mejora de la imagen: suavizado
 	smoothImage = gaussianFilter2D (inputImage, sigma);
 	smoothImage = uExtendShrink (smoothImage, gaussKernel2D(sigma), operation, fillValue);
 	
-	% Localizaci√≥n de bordes empleando el detector de Sobel
+	% LocalizaciÛn de bordes empleando el detector de Sobel
 	[gy, gx] = derivatives (smoothImage, 'Sobel');
 
-	% Recorta los bordes bordes que se a√±aden la convoluci√≥n tipo full
+	% Recorta los bordes bordes que se aÒaden la convoluciÛn tipo full
 	gy = uExtendShrink (gy, operator, operation, fillValue);
 	gx = uExtendShrink (gx, operator, operation, fillValue);
 
@@ -148,22 +157,37 @@ function outputImage = edgeCanny (inputImage, sigma, tlow, thigh)
 	% Matriz de orientaciones
 	Eo = atan (gy ./ gx); % OjO: result in radians.
 	Eo = radtodeg (Eo); % Cambia los valores de radianes a grados
-	Eo = mod (Eo+360, 180); % Pasamos todos los √°ngulos al primer y segundo cuadrantes
+	Eo = mod (Eo+360, 180); % Pasamos todos los ·ngulos al primer y segundo cuadrantes
 	
 	% 4 grupos dk de orientaciones
-	dk1 = (Eo>=0 & Eo<45);     % 0¬∫ to 45¬∫ -----> -
-    dk2 = (Eo>=45 & Eo<90);   % 45¬∫ to 90¬∫ ----> /
-    dk3 = (Eo>=90 & Eo<135);  % 90¬∫ to 135¬∫  --> |
-    dk4 = (Eo>=135 & Eo<180); % 135¬∫ to 180¬∫ --> \
+	dk1 = (Eo>=0 & Eo<45);     % 0∫ to 45∫ -----> -
+    dk2 = (Eo>=45 & Eo<90);   % 45∫ to 90∫ ----> /
+    dk3 = (Eo>=90 & Eo<135);  % 90∫ to 135∫  --> |
+    dk4 = (Eo>=135 & Eo<180); % 135∫ to 180∫ --> \
 	
-	% supresi√≥n no m√°xima - mirando s√≥lo los vecinos de cada punto
-	outputImage = supresion (outputImage, Em, 'V', dk1, r, c);
-	outputImage = supresion (outputImage, Em, 'I', dk2, r, c);
-	outputImage = supresion (outputImage, Em, 'H', dk3, r, c);
-	outputImage = supresion (outputImage, Em, 'D', dk4, r, c);
+	% supresiÛn no m·xima - mirando sÛlo los vecinos adyacentes de cada punto
+	output = supresion (output, Em, 'V', dk1, r, c);
+	output = supresion (output, Em, 'I', dk2, r, c);
+	output = supresion (output, Em, 'H', dk3, r, c);
+	output = supresion (output, Em, 'D', dk4, r, c);
+	outputImage = output;
+	% desactiva todos los puntos con valor inferior a tlow
+	output(output<=tlow) = 0;
 	
-	% umbralizaci√≥n por hist√©resis
+	% matriz de unos para los objetos que est·n entre tlow y thigh
+	midHigh = (output > tlow);
 	
+	% num componentes conexas en la matriz label
+	[labels, num] = bwlabel (midHigh, 8);
+	disp(sprintf('[edgeCanny] n∫ de componentes conexas: %d', num))
 	
+	% matriz de tamaÒo r,c con unos donde hay thigh
+	rangoAlto = output >= thigh;
 	
+	% para cada coordenada de thigh
+	for phigh = find(output>thigh)
+		for label = labels(phigh)' % etiquetas donde hay y thigh
+			outputImage (find(labels == label)) = 1;
+		end
+	end
 end
