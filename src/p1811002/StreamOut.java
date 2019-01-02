de44927daf4373278a6360909f1d71ad7087b7f3
Diego Trabazo Sardón <dtrabazo@gmail.com>
2019-01-02 21:24:06 +0100

    Hace uso de la interfaz StreamOut

---
+++ src/p1811002/StreamOut.java
@@ -0,0 +1,54 @@
+package p1811002;
+
+import it.unimi.dsi.io.OutputBitStream;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.List;
+
+public interface StreamOut {
+  public void write(int [] a, int start, int count);
+  public void write(List<Integer> data);
+  public void close();
+
+  class DeltaStreamOut implements StreamOut {
+
+    private final OutputBitStream outputBitStream;
+
+    public DeltaStreamOut(OutputStream outputStream) {
+      outputBitStream = new OutputBitStream(outputStream);
+    }
+
+    public void write(int[] a, int start, int count) {
+
+      for (int i = start; i < start + count; i++) {
+        try {
+          outputBitStream.writeDelta(a[i]);
+
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    public void write(List<Integer> data) {
+      Iterator<Integer> iterator = data.iterator();
+      while (iterator.hasNext()) {
+        try {
+          outputBitStream.writeDelta(iterator.next());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    public void close() {
+      try {
+        outputBitStream.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
