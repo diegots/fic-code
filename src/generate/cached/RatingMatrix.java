bf53eedebf2b8420abd3941232adaa9e0dfd2fbc
Diego Trabazo Sardón <dtrabazo@gmail.com>
2019-01-10 11:53:28 +0100

    Split dataset into n shards
    An User abstraction is used to represent an userId and his profile.

--- src/generate/cached/RatingMatrix.java
+++ src/generate/cached/RatingMatrix.java
@@ -1,12 +1,39 @@
 package generate.cached;
 
+import generate.Conf;
+import generate.dataset.Dataset;
+import generate.dataset.User;
+import generate.utils.Utilities;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Prepare data to be proccessed in Hadoop. Reorganizes the data according how
  * is going to be accessed.
  */
 public class RatingMatrix {
 
-  // Idea here is to be able to group users the same way are going to be accessed from Hadoop.
+  private final Dataset dataset;
 
-  // Then write data into a new file
+  public RatingMatrix(Dataset dataset) {
+    this.dataset = dataset;
+  }
+
+  public void distribureUsersToShards() {
+
+    Set<Integer> userIds = dataset.getUserIds();
+    Iterator<Integer> iterator = userIds.iterator();
+    while (iterator.hasNext()) {
+
+      int userId = iterator.next();
+      Map<Integer, Double> userProfile = dataset.getUserProfile(userId);
+
+      String shardPath = Conf.getConf().getRatingMatrixPath()
+          + userId % Conf.getConf().getShardsNumber();
+
+      Utilities.objectToFile(shardPath, new User(userId, userProfile));
+    }
+  }
 }
---
+++ src/generate/dataset/User.java
@@ -0,0 +1,24 @@
+package generate.dataset;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Map;
+
+// TODO Utilize user instead of Map<Integer, Map<Integer, Double>>
+public class User implements Serializable {
+  private final int userId;
+  private final Map<Integer, Double> userProfile;
+
+  public User(int userId, Map<Integer, Double> userProfile) {
+    this.userId = userId;
+    this.userProfile = userProfile;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public Map<Integer, Double> getUserProfile() {
+    return Collections.unmodifiableMap(userProfile);
+  }
+}
