b1135cc73a9bf272a03d1d531deb71730c7bde33
Diego Trabazo Sardón <dtrabazo@gmail.com>
2019-02-26 10:27:20 +0100

    Improves User class and adds tests

--- src/main/java/tfg/common/model/User.java
+++ src/main/java/tfg/common/model/User.java
@@ -1,8 +1,7 @@
 package tfg.common.model;
 
 import java.io.Serializable;
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 
 public class User implements Serializable {
   private final int userId;
@@ -10,15 +9,19 @@ public class User implements Serializable {
 
   public User(int userId, Map<Integer, Double> userProfile) {
     this.userId = userId;
-    this.userProfile = userProfile;
+
+    if (userProfile == null)
+      this.userProfile = new HashMap<>();
+    else
+      this.userProfile = userProfile;
   }
 
   public static User getEmptyUser() {
-    return new User(0, null);
+    return new User(0, new HashMap<>());
   }
 
   public static boolean isEmptyUser(User user) {
-    return 0 == user.getUserId() && null == user.userProfile;
+    return 0 == user.getUserId() && user.userProfile.size() == 0;
   }
 
   public int getUserId() {
@@ -27,5 +30,20 @@ public class User implements Serializable {
 
   public Map<Integer, Double> getUserProfile() {
     return Collections.unmodifiableMap(userProfile);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+
+    return userId == user.userId &&
+        userProfile.equals(user.userProfile);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(userId, userProfile);
   }
 }
