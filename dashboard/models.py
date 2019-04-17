d431da42df68e87583d44324865ba6d28c525f75
Diego Trabazo Sardón <dtrabazo@gmail.com>
2019-04-17 12:30:24 +0200

    Define str method for Cluster class

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Cluster(models.Model):
     cluster_id = models.CharField(max_length=30) # cluster id example: j-1LE1XBWJF3OCF
     number_nodes = models.IntegerField()
+
+    def __str__(self):
+        return 'aws emr id: ' + self.cluster_id + ', with ' + str(self.number_nodes) + ' nodes'
