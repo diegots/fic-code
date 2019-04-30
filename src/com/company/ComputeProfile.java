a3bc601dbc4a85123f8ec196e7e6a552bf174d44
Diego Trabazo Sardón <dtrabazo@gmail.com>
2019-04-30 17:14:19 +0200

    Implement profile extracting multithread

---
+++ src/com/company/ComputeProfile.java
@@ -0,0 +1,104 @@
+package com.company;
+
+import java.io.*;
+
+public class ComputeProfile extends Thread {
+    private Thread t;
+    private String threadName;
+    private int startId;
+    private int endId;
+
+    public ComputeProfile(String threadName, int startId, int endId) {
+        this.threadName = threadName;
+        this.startId = startId;
+        this.endId = endId;
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println("Running thread " + threadName);
+
+        /**
+         * Abre ficheros
+         */
+        BufferedWriter writer = null;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(Main.input));
+            writer = new BufferedWriter(new FileWriter(new File(threadName+Main.outFile), false));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //////////////////////
+        String line;
+        StringBuilder profile = new StringBuilder();
+        Double denom = 0.0;
+        try {
+            while ((line = reader.readLine()) != null) {
+                int userId = Integer.valueOf(line.split(Main.separator)[0]);
+                if (userId < startId) {
+                    continue;
+                } else if (userId > endId) {
+
+                    if (profile.length() > 0) { // Último usuario, volcar a disco
+                        writer.append(profile.append(Main.separator + denom).append("\n").toString());
+                    }
+
+                    break;
+                } else {
+
+                    String itemId = line.split(Main.separator)[1];
+                    String rating = line.split(Main.separator)[2];
+
+                    if (profile.length() == 0) { // No tengo nada. Primera linea del fichero
+                        profile.append(userId);
+                        profile.append(Main.separator + itemId + Main.separator + rating);
+                        denom += Double.valueOf(rating) * Double.valueOf(rating);
+
+                    } else { // Ya hay información previa
+                        if (profile.toString().split(Main.separator)[0].equals(""+userId)) { // Mismo usuario
+                            profile.append(Main.separator + itemId + Main.separator + rating);
+                            denom += Double.valueOf(rating) * Double.valueOf(rating);
+
+                        } else { // Distinto usuario
+
+                            // Almacenar la información en fichero
+                            writer.append(profile.append(Main.separator + denom).append("\n").toString());
+
+                            // Guardar nueva cadena
+                            profile = new StringBuilder().append(userId);
+                            profile.append(Main.separator + itemId + Main.separator + rating);
+                            denom = Double.valueOf(rating) * Double.valueOf(rating);
+
+                            //System.out.printf(".");
+                        }
+                    }
+                }
+            }
+
+            if (line == null) { // Trata la última linea del fichero
+                writer.append(profile.append(Main.separator + denom).append("\n").toString());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Cierra ficheros
+         */
+        try {
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        System.out.println("Thread " + threadName + " exiting");
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,26 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
     static String separator = ",";
+    static String outProfile;
     static File input;
+
     static File outProfiles;
     static File outFile;
 
     public static void main(String[] args) {
 
         input = new File(args[0]);
+        outProfile = args[1];
+
+
+        int threadsNumber = Integer.valueOf(args[2]);
         outProfiles = new File("profiles-" + args[1]);
         outFile = new File(args[1]);
 
@@ -42,143 +49,40 @@ public class Main {
 
 
         /**
-         *
+         * Calcular perfiles de usuario
          */
-        List<Integer> itemIds;
-        List<Double> ratings;
-
-        /**
-         * Calcula perfiles
-         */
-        System.out.print("Calcula perfiles");
-        BufferedWriter bufferedWriter = null;
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter(outProfiles, false));
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (threadsNumber > maxUserId) {
+            System.err.println("Thread number can't be greater than userIds.");
             System.exit(1);
         }
 
-        for (int userId = 1; userId<=maxUserId; userId++) {
-            itemIds = new LinkedList<>();
-            ratings = new LinkedList<>();
+        List<ComputeProfile> threads = new ArrayList<>();
+        int step = maxUserId / threadsNumber;
+        System.out.println("step: " + step);
+        for (int i = 0; i<threadsNumber; i++) {
+            System.out.println("f: " + (step * i + 1) + " to: " + (step * (i+1)));
+            int from = step * i + 1;
+            int to = step * (i+1);
+            threads.add(new ComputeProfile(""+i, from, to));
+        }
 
-            /**
-             * Leer perfil del usuario
-             */
-            Double denom = 0.0;
+        if (maxUserId % threadsNumber != 0) {
+            System.out.println("f: " + (step * threadsNumber + 1) + " to: " + (maxUserId));
+            threads.add(new ComputeProfile("" + threadsNumber, (step * threadsNumber + 1), maxUserId));
+        }
+
+        for (ComputeProfile thread: threads) {
+            thread.start();
+        }
+
+        for (ComputeProfile thread: threads) {
             try {
-                BufferedReader br = new BufferedReader(new FileReader(input));
-                while ((readLine = br.readLine()) != null) {
-                    if (Integer.valueOf(readLine.split(separator)[0]) == userId) {
-                        itemIds.add(Integer.valueOf(readLine.split(separator)[1]));
-                        Double rating = Double.valueOf(readLine.split(separator)[2]);
-                        ratings.add(rating);
-                        denom += rating * rating;
-                    }
-                }
-                br.close();
-                System.out.print(".");
-            } catch (FileNotFoundException e) {
+                thread.join();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
-                System.exit(1);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-
-            /**
-             * Escribir perfil a disco
-             */
-            StringBuilder line = new StringBuilder();
-            line.append(userId + "," + Math.sqrt(denom) + ",");
-            for (int i=0; i<itemIds.size(); i++) {
-                line.append(itemIds.get(i) + "," + ratings.get(i) + ",");
-            }
-            line.append("\n");
-
-            try {
-                bufferedWriter.append(line);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(1);
             }
         }
 
-        /**
-         * Cierra fichero de perfiles
-         */
-        try {
-            bufferedWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        /**
-         * Calcula similaridades
-         */
-        System.out.print("\nCalcula similaridades");
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter(outFile, false));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        String lineA;
-        String lineB;
-        try {
-            BufferedReader brA = new BufferedReader(new FileReader(outProfiles));
-            while ((lineA = brA.readLine()) != null) {
-                String [] valuesA = lineA.split(separator);
-                Integer userIdA = Integer.valueOf(valuesA[0]);
-
-
-                try {
-                    BufferedReader brB = new BufferedReader(new FileReader(outProfiles));
-                    while ((lineB = brB.readLine()) != null) {
-                        String[] valuesB = lineB.split(separator);
-                        Integer userIdB = Integer.valueOf(valuesB[0]);
-
-                        Double sum = 0.0;
-                        if (userIdB > userIdA) {
-                            for (int itemB = 2; itemB<valuesB.length; itemB+=2) {
-
-                                for (int itemA = 2; itemA<valuesA.length; itemA+=2) {
-
-                                    if (valuesA[itemA].equals(valuesB[itemB])) {
-                                        sum += Double.valueOf(valuesA[itemA+1]) * Double.valueOf(valuesB[itemB+1]);
-                                    }
-                                }
-                            }
-
-                            bufferedWriter.append(userIdA + "\t" + userIdB + "\t"
-                                    + sum/(Double.valueOf(valuesA[1]) + Double.valueOf(valuesB[1])) + "\n");
-                        }
-                    }
-                    brB.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
-                System.out.print(".");
-            }
-            brA.close();
-            bufferedWriter.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.exit(1);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-
+        System.out.println("Exiting main");
     }
 }
