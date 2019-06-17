c21014fccf205af57e376c7bd0c3ff1f638967ca
Diego Trabazo Sardón <dtrabazo@gmail.com>
2019-06-17 11:42:21 +0200

    Put writer code into functions

--- src/main/java/tfg/SortSimilarities.java
+++ src/main/java/tfg/SortSimilarities.java
@@ -14,36 +14,24 @@ public class SortSimilarities extends Task {
     public void run() {
 
         System.out.println("Running thread " + getThreadName() + ", min: " + getMin() + ", max: " + getMax());
-        BufferedWriter writer = null;
+
+        Writer writer = buildWriterFromFile(new File("sorted"+getThreadName()));
+        if (writer == null) {
+            return;
+        }
+
         BufferedReader reader;
-        String line;
         Integer numberFiles = getContext().getInteger(Context.THREADS_NUMBER, 0);
         int usersPerStep = getContext().getInteger(Context.USERS_PER_STEP, -1);
         String separator = getContext().getString(Context.SEPARATOR, "");
+        List<Map<Integer, Integer>> usersMaps = initializeUsersMaps(usersPerStep);
 
-        try {
-            writer = new BufferedWriter(new FileWriter(new File("sorted"+getThreadName()), false));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        List<Map<Integer, Integer>> usersMaps = new ArrayList<>(usersPerStep);
-        for (int i=0; i<usersPerStep; i++) {
-            usersMaps.add(new TreeMap<>(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer integer, Integer t1) {
-                    return (integer.compareTo(t1));
-                }
-            }));
-        }
-
-
+        String line;
         for (int userId = getMin(); userId<=getMax(); userId+=usersPerStep) {
 
-            System.out.println("thread: " + getThreadName() + " - " + "min: " + getMin() + " - " + "max: " + getMax());
+
             for (int fileCounter = 0; fileCounter<numberFiles; fileCounter++) {
-                System.out.println("thread: " + getThreadName() + " - " + "file: " + fileCounter);
+
                 try {
                     reader = new BufferedReader(new FileReader("similarity"+fileCounter), 1000 * 8192);
                     while ((line = reader.readLine()) != null) {
@@ -52,7 +40,7 @@ public class SortSimilarities extends Task {
                         int userB = Integer.valueOf(lineContents[1]);
 
                         for (int i=0; (i+userId) <= getMax() && i < usersPerStep; i++) {
-                            //System.out.println("thread: " + getThreadName() + " - " + "userId: " + (i+userId));
+
                             Map<Integer, Integer> map = usersMaps.get(i);
                             if (userA == i+userId) {
                                 map = addElement(map, userB, lineContents[2]);
@@ -76,19 +64,11 @@ public class SortSimilarities extends Task {
                     sb.append(",").append(map.get(l.get(j)));
                 }
 
-                try {
-                    writer.append(sb.append("\n").toString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                writeContentToFile(writer, sb.append("\n").toString());
             }
         } // end userId for loop
 
-        try {
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        closeWriter(writer);
     }
 
 
@@ -109,5 +89,46 @@ public class SortSimilarities extends Task {
         }
 
         return map;
+    }
+
+
+    Writer buildWriterFromFile(File destination) {
+        try {
+            return new BufferedWriter(new FileWriter(destination));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    List<Map<Integer, Integer>> initializeUsersMaps(int mapSize) {
+
+        List<Map<Integer, Integer>> maps = new ArrayList<>(mapSize);
+        for (int i=0; i<mapSize; i++) {
+            maps.add(new TreeMap<>(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer integer, Integer t1) {
+                    return (integer.compareTo(t1));
+                }
+            }));
+        }
+        return maps;
+    }
+
+    void closeWriter(Writer writer) {
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void writeContentToFile(Writer writer, String contents) {
+        try {
+            writer.append(contents);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
