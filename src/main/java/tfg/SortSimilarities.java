9bb3b9aaef634638e8373a9502a6fbf199f27ed0
Diego Trabazo Sardón <dtrabazo@gmail.com>
2019-06-17 16:23:34 +0200

    Fix wrong Comparator, bad constant retrieval

--- src/main/java/tfg/SortSimilarities.java
+++ src/main/java/tfg/SortSimilarities.java
@@ -5,6 +5,14 @@ import java.util.*;
 
 public class SortSimilarities extends Task {
 
+
+    final static int userAPosition = 0;
+    final static int userBPosition = 0;
+    final static int similarityPosition = 0;
+
+    int usersPerStep = getContext().getInteger(Context.USERS_PER_STEP, -1);
+
+
     public SortSimilarities(TaskData taskData, Context context) {
         super(taskData, context);
     }
@@ -22,13 +30,12 @@ public class SortSimilarities extends Task {
 
         BufferedReader reader;
         Integer numberFiles = getContext().getInteger(Context.THREADS_NUMBER, 0);
-        int usersPerStep = getContext().getInteger(Context.USERS_PER_STEP, -1);
+
         String separator = getContext().getString(Context.SEPARATOR, "");
-        List<Map<Integer, Integer>> usersMaps = initializeUsersMaps(usersPerStep);
+        List<Map<Integer, Integer>> usersMaps = initListOfTreeMapWithSize(usersPerStep);
 
         String line;
         for (int userId = getMin(); userId<=getMax(); userId+=usersPerStep) {
-
 
             for (int fileCounter = 0; fileCounter<numberFiles; fileCounter++) {
 
@@ -36,16 +43,17 @@ public class SortSimilarities extends Task {
                     reader = new BufferedReader(new FileReader("similarity"+fileCounter), 1000 * 8192);
                     while ((line = reader.readLine()) != null) {
                         String [] lineContents = line.split(separator);
-                        int userA = Integer.valueOf(lineContents[0]);
-                        int userB = Integer.valueOf(lineContents[1]);
+                        int userA = Integer.valueOf(lineContents[userAPosition]);
+                        int userB = Integer.valueOf(lineContents[userBPosition]);
+                        int similarity = Integer.valueOf(lineContents[similarityPosition]);
 
                         for (int i=0; (i+userId) <= getMax() && i < usersPerStep; i++) {
 
                             Map<Integer, Integer> map = usersMaps.get(i);
                             if (userA == i+userId) {
-                                map = addElement(map, userB, lineContents[2]);
+                                map = addElement(map, userB, similarity);
                             } else if (userB == i+userId) {
-                                map = addElement(map, userA, lineContents[2]);
+                                map = addElement(map, userA, similarity);
                             }
                         }
                     }
@@ -55,31 +63,21 @@ public class SortSimilarities extends Task {
                     e.printStackTrace();
                 }
             } // end file counter for loop
+            writeUserNeighbors(userId, writer, usersMaps);
 
-            for (int i=0; i+userId <= getMax() && i < usersPerStep; i++) {
-                Map<Integer, Integer> map = usersMaps.get(i);
-                StringBuilder sb = new StringBuilder().append(i+userId);
-                List<Integer> l = new ArrayList<>(map.keySet());
-                for (int j=l.size()-1; j>=0; j--) {
-                    sb.append(",").append(map.get(l.get(j)));
-                }
-
-                writeContentToFile(writer, sb.append("\n").toString());
-            }
         } // end userId for loop
 
         closeWriter(writer);
     }
 
 
-    private Map<Integer, Integer> addElement(Map<Integer, Integer> map, int userId, String similarity) {
+    Map<Integer, Integer> addElement(Map<Integer, Integer> map, int userId, Integer similarity) {
 
-        int threshold = getContext().getInteger(Context.SEPARATOR, -1);
-        int neighborhoodSize = getContext().getInteger(Context.NEIGHBORHOOD_SIZE, -1);
+        Integer threshold = getContext().getInteger(Context.SIMILARITY_THRESHOLD, null);
+        Integer neighborhoodSize = getContext().getInteger(Context.NEIGHBORHOOD_SIZE, null);
 
-        Integer s = Integer.valueOf(similarity);
-        if (s > threshold) {
-            map.put(s, userId);
+        if (similarity > threshold) {
+            map.put(similarity, userId);
             if (map.size() > neighborhoodSize) {
                 for (Integer i: map.keySet()) {
                     map.remove(i);
@@ -102,19 +100,20 @@ public class SortSimilarities extends Task {
     }
 
 
-    List<Map<Integer, Integer>> initializeUsersMaps(int mapSize) {
+    static List<Map<Integer, Integer>> initListOfTreeMapWithSize(int size) {
 
-        List<Map<Integer, Integer>> maps = new ArrayList<>(mapSize);
-        for (int i=0; i<mapSize; i++) {
+        List<Map<Integer, Integer>> maps = new ArrayList<>(size);
+        for (int i=0; i<size; i++) {
             maps.add(new TreeMap<>(new Comparator<Integer>() {
                 @Override
                 public int compare(Integer integer, Integer t1) {
-                    return (integer.compareTo(t1));
+                    return (t1.compareTo(integer));
                 }
             }));
         }
         return maps;
     }
+
 
     void closeWriter(Writer writer) {
         try {
@@ -124,11 +123,27 @@ public class SortSimilarities extends Task {
         }
     }
 
+
     void writeContentToFile(Writer writer, String contents) {
         try {
             writer.append(contents);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    void writeUserNeighbors (int userId, Writer writer, List<Map<Integer, Integer>> usersMaps) {
+
+        for (int i=0; i+userId <= getMax() && i < usersPerStep; i++) {
+            Map<Integer, Integer> map = usersMaps.get(i);
+            StringBuilder sb = new StringBuilder().append(i+userId);
+            List<Integer> l = new ArrayList<>(map.keySet());
+            for (int j=l.size()-1; j>=0; j--) {
+                sb.append(",").append(map.get(l.get(j)));
+            }
+
+            writeContentToFile(writer, sb.append("\n").toString());
         }
     }
 }
