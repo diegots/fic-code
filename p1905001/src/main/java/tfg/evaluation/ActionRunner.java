68350b1fcc7ed54a54afef785216a25ff3095b67
Diego Trabazo Sardón <dtrabazo@gmail.com>
2019-07-01 16:35:30 +0200

    Fix ActionHelp presence checking

--- p1905001/src/main/java/tfg/evaluation/ActionRunner.java
+++ p1905001/src/main/java/tfg/evaluation/ActionRunner.java
@@ -12,7 +12,7 @@ public class ActionRunner {
     boolean isHelpActionArgumentPresent(List<Action> actions) {
 
         for (Action action: actions) {
-            if (new Action.ActionHelp().equals(action)) {
+            if (action.getClass().getName().equals(Action.ActionHelp.class.getName())) {
                 return true;
             }
         }
@@ -21,9 +21,11 @@ public class ActionRunner {
     }
 
 
-    private void getValidationDone(List<Action> actions) {
+    void getValidationDone(List<Action> actions) {
+
         if (isEmptyActionArgument(actions)) {
             actions.add(new Action.ActionEmpty("Ups! no actions requested. Try with -h."));
+
         } else if (isHelpActionArgumentPresent(actions)) {
             actions.clear();
             actions.add(new Action.ActionHelp());
